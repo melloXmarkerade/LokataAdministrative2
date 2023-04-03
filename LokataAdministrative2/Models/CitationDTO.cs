6489a4fc55a1695ce9123b163d77747e545de67a
markerade2 <markerade234@gmail.com>
2023-04-03 17:51:24 +0800

    Add project files.

---
+++ LokataAdministrative2/Models/CitationDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace LokataAdministrative2.Models
+{
+    public class CitationDTO
+    {
+        public string? LicesnseNo { get; set; }
+        public string? LastName { get; set; }
+        public string? FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string? LicenseType { get; set; }
+        public string? Officer { get; set; }
+        public string? Address { get; set; }
+        public string? VehicleDescription { get; set; }
+        public string? PlaceApprehended { get; set; }
+        public string? Violations { get; set; }
+
+    }
+}
---
+++ LokataAdministrative2/Services/IClient.cs
@@ -0,0 +1,7 @@
+﻿namespace LokataAdministrative2.Services
+{
+    public interface IClient<T>
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+    }
+}
